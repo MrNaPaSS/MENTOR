d12800c2de11005c8c0dd6bfe30cf3aa73255c0e
MrNaPaSS <kaktotakxm00@gmail.com>
2026-06-17 21:37:25 +0200

    fix: use next/dynamic for signals [id] page to fix static export detection

--- webapp/app/app/signals/[id]/page.tsx
+++ webapp/app/app/signals/[id]/page.tsx
@@ -1,4 +1,6 @@
-import SignalDetail from "./SignalDetail";
+import dynamic from "next/dynamic";
+
+const SignalDetail = dynamic(() => import("./SignalDetail"), { ssr: false });
 
 export async function generateStaticParams() {
   return [];
